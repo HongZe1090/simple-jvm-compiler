e69c63c421990491fe88e8330e4aeb98fc9772f7
1090414202 <1090414202@qq.com>
2021-09-26 23:49:26 +0800

    初始话命令行类

---
+++ jvmgo/cmd.go
@@ -0,0 +1,39 @@
+package jvmgo
+
+import "flag"
+import "fmt"
+import "os"
+
+// 结构体表示命令行参数和选项
+type Cmd struct {
+	helpFlag bool
+	versionFlag bool
+	cpOption string
+	class string
+	args []string
+ }
+
+//  对命令的执行
+func parseCmd *Cmd {
+	cmd :=&Cmd() //初始化Cmd结构体
+
+	flag.Usage = printUsage //在解析失败时调用printUsage
+	flag.BoolVar(&cmd.helpFlag , "help",false,"print help message")
+	flag.BoolVar(&cmd.helpFlag,"?",false,"print help message")
+	flag.StringVar(&cmd.cpOption,"classpath","","classpath")
+	flag.StringVar(&cmd.cpOption,"cp","","classpath")	
+	flag.Prase()
+
+	args :=flag.Args() //获取参数数组
+	if len(agrs) > 0{
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+
+	return cmd
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+	//flag.PrintDefaults()
+}
